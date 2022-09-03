b424295e45273a17ef6ba79366624f360b74848f
a-fralou <afralou@yandex.by>
2022-09-04 00:41:54 +0300

    Update

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,27 +2,58 @@ import '@popperjs/core';
 import 'bootstrap/js/dist/modal';
 import IMask from 'imask';
 
-const exampleModal = document.getElementById('exampleModal')
-exampleModal.addEventListener('show.bs.modal', event => {
+// const exampleModal = document.getElementById('exampleModal')
+// exampleModal.addEventListener('show.bs.modal', event => {
 
-  const button = event.relatedTarget
+//   const button = event.relatedTarget
 
-  const recipient = button.getAttribute('data-bs-whatever')
+//   const recipient = button.getAttribute('data-bs-whatever')
 
-  const modalTitle = exampleModal.querySelector('.modal-title')
-  const modalBodyInput = exampleModal.querySelector('.modal-body input')
+//   const modalTitle = exampleModal.querySelector('.modal-title')
+//   const modalBodyInput = exampleModal.querySelector('.modal-body input')
 
-  modalTitle.textContent = `New message to ${recipient}`
+//   modalTitle.textContent = `New message to ${recipient}`
 
-});
+// });
 
-var element = document.getElementById('recipient-name');
+var phoneField = document.getElementById('phone-field');
 
-var maskOptions = {
+var phoneFieldOptions = {
   mask: '+{375} (00) 000-00-00',
   lazy: false,
 };
 
-IMask(element, maskOptions);
+IMask(phoneField, phoneFieldOptions);
+
+
+var codeField = document.getElementById('code-field');
+
+var codeFieldOptions = {
+  mask: '000000',
+  lazy: false,
+};
+
+IMask(codeField, codeFieldOptions);
+
+
+
+var phoneField1 = document.getElementById('phone-field1');
+
+var phoneField1Options = {
+  mask: '+{375} (00) 000-00-00',
+  lazy: false,
+};
+
+IMask(phoneField1, phoneField1Options);
+
+
+var codeField1 = document.getElementById('code-field1');
+
+var codeField1Options = {
+  mask: '000000',
+  lazy: false,
+};
+
+IMask(codeField1, codeField1Options);
 
 // https://imask.js.org/guide.html
